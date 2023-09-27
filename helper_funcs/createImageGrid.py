59d713c1d9a0422553b64d52c9743cf86f1797f9
joe <joseph.satow@gmail.com>
2023-09-27 22:06:41 +0000

    working with grid in proper order

--- helper_funcs/createImageGrid.py
+++ helper_funcs/createImageGrid.py
@@ -14,13 +14,28 @@ def createImage(chartSaveFolder):
     transform = transforms.Compose([
         transforms.ConvertImageDtype(dtype=torch.float),
     ])
+    
+    rearrangedList = rearrangeFileList(os.listdir(f"{homePath}/images/{chartSaveFolder}"))
 
-    for file in os.listdir(f"{homePath}/images/{chartSaveFolder}"):
+    for file in rearrangedList:
         if file.endswith(".png"):
             image = os.path.join(f"{homePath}/images/{chartSaveFolder}", file)
             transformed_tensor = transform(read_image(image))
             tensors.append(transformed_tensor)
 
-    grid = make_grid(tensors, nrow=2, padding=2)
+    grid = make_grid(tensors, nrow=2, padding=1)
 
     save_image(grid, f"{homePath}/images/results/grid-{chartSaveFolder}.jpg")
+
+def rearrangeFileList(fileList):
+    newList = [None] * 4
+    for filename in fileList:
+        if "1w" in filename:
+            newList[0] = filename
+        if "1d" in filename:
+            newList[1] = filename
+        if "4h" in filename:
+            newList[2] = filename
+        if "1h" in filename:
+            newList[3] = filename
+    return newList
